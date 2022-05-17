8a7f3f0b57fe920132b552f9a7213fe8bd8c67fd
0xKubitus <n.cubilier@gmail.com>
2022-05-17 14:15:10 +0200

    UserMailer créé mais ajouts à faire avant de tester

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,26 @@
+class UserMailer < ApplicationMailer
+  #default from: 'n.cubilier@gmail.com'
+
+  def welcome_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'https://eventproject-mardi-kubitus.herokuapp.com/' # <= page d'accueil mon site, à modifier vers une page 'login'
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Bienvenue chez The Events Project by Kubitus ! (compte utilisateur créé)') 
+  end
+
+  def admin_booking_email(attendance) 
+    @admin = attendance.event.admin
+    @attendee = attendance.attendee
+    @event = attendance.event
+    @url  = 'https://eventproject-mardi-kubitus.herokuapp.com/'
+    mail(to: @admin.email, subject: 'Une réservation a été passée sur ton Event !') 
+  end
+
+
+end # fin class UserMailer
+class UserMailer < ApplicationMailer
+end
