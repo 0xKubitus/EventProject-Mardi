f60efbbb2ca6322ef6aee256a600ee7aeae9e2f3
0xKubitus <n.cubilier@gmail.com>
2022-05-17 13:58:17 +0200

    ajouts des liens aux models + debut de validations

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,2 +1,22 @@
 class Attendance < ApplicationRecord
-end
+  after_create :admin_booking_send #permet de lancer automatiquement cette methode quand une réservation est effectuée (ce qui se traduit par la creation en base d'une 'attendance' à un 'event' par un 'user/attendee')
+
+  #validates :stripe_customer_id,
+   #presence: true,
+   #format: .....???.....
+
+   
+  belongs_to :attendee, class_name: "User"
+  belongs_to :event
+
+
+  def admin_booking_send
+    UserMailer.admin_booking_email(self).deliver_now
+    puts " "
+    puts "* - *"*50
+    puts "Résa validé -> email envoyé au user/admin"
+    puts "* - "*50
+    puts " "
+  end
+
+end # fin class 'Attendance'
