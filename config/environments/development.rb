95d406b6f2325a1a1b858f84cb6bb571f0364360
0xKubitus <n.cubilier@gmail.com>
2022-05-17 14:32:59 +0200

    moment de révélation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
     #LETTER_OPENER
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
