731c8d91be2e082d292c6eb24625217bf5322eee
0xKubitus <n.cubilier@gmail.com>
2022-05-20 01:10:18 +0200

    j'ai suivi la video du corsaire de THP sur le checkout mais ça ne fonctionne pas : je n'ai pas de formulaire qui s'ouvre pour effectuer le paiement, mais pas de message d'erreur non plus !

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,40 @@
 class OrdersController < ApplicationController
-  def new
-  end
 
   def create
+    @user = current_user
+    @total = params[:total].to_d
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          name: 'Rails Stripe Checkout au top, tu connais ! ;)',
+          amount: (@total*100).to_i,
+          currency: 'eur',
+          quantity: 1
+        },
+      ],
+      success_url: order_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: order_cancel_url # on peut rediriger ailleurs si on veut, par exemple vers 'root_url' plutot qu'une page 'order_cancel'
+      #on peut aussi ajouter un flash 'Transaction annulée - erreur lors du paiement' en cas d'erreur 
+    )  
+    respond_to do |format|
+      format.js # renders create.js.erb
+    end
+  end # fin methode 'create'
+
+
+  def success
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
   end
-end
+
+  def cancel
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+  end
+
+
+
+
+
+end # fin classe OrdersController
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :orders, only: [:create]
+  get 'cancel', to: 'order#cancel', as: 'order_cancel'
+  get 'success', to: 'order#success', as: 'order_success'
+
+
   root to: 'events#index'
 
   devise_for :users
-
-  get 'static_pages/index'
-  get 'static_pages/secret'
 
   
   resources :users 
@@ -15,11 +17,9 @@ Rails.application.routes.draw do
   end
 
 
-
-  
-  
-  
-  
+  #les 2 lignes ne sont pas utiles - peuvent être supprimées (les 2 pages qui y sont liées ne servent à rien)
+  get 'static_pages/index'
+  get 'static_pages/secret'
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
